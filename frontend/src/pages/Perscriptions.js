0d124fbd60af4aa40996917bf5ae9009e3bbd006
AmrKhaledelsharkawy <amrk13415@gmail.com>
2023-10-14 01:06:10 +0300

    Perscription prob

--- frontend/src/pages/Perscriptions.js
+++ frontend/src/pages/Perscriptions.js
@@ -1,71 +1,70 @@
-import { useEffect, useState } from 'react'
-import DoctorDetails from '../components/DoctorDetails';
+import { useEffect, useState } from 'react';
 import PerscriptionDetails from '../components/PerscriptionDetails';
 
 const Perscription = () => {
-
     const [perscriptions, setPerscription] = useState(null);
     const [dateFilter, setDateFilter] = useState('');
     const [doctorFilter, setDoctorFilter] = useState('');
     const [stateFilter, setStateFilter] = useState('');
 
     useEffect(() => {
-      const fetchPerscription = async () => {
-        let url = '/api/perscription';
+        const fetchPerscription = async () => {
+            let url = '/api/perscription';
 
-        const params = new URLSearchParams();
-        if (dateFilter) params.append('date', dateFilter);
-        if (doctorFilter) params.append('doctor', doctorFilter);
-        if (stateFilter) params.append('state', stateFilter);
-        if (params.toString()) url += `?${params.toString()}`;
+            const params = new URLSearchParams();
+            if (dateFilter) params.append('date', dateFilter);
+            if (doctorFilter) params.append('doctorName', doctorFilter);
+            if (stateFilter) params.append('state', stateFilter);
+            if (params.toString()) url += `?${params.toString()}`;
 
-        console.log(url)
-        const response = await fetch(url);
-        const json = await response.json();
+            
+            const response = await fetch(url);
+            const json = await response.json();
 
-        if (response.ok) {
-            setPerscription(json)
-            console.log(json);
-        }
-      }
-      fetchPerscription();
-    }, [dateFilter, doctorFilter, stateFilter])
+            if (response.ok) {
+              
+                    setPerscription(json);
+                
+            }
+        };
+        fetchPerscription();
+    }, [dateFilter, doctorFilter, stateFilter]);
 
     return (
-      <div className="doctors">
-        {/* Filter section */}
-        <div className="filters">
-          <input 
-            type="text" 
-            placeholder="Filter by Date" 
-            value={dateFilter}
-            onChange={(e) => setDateFilter(e.target.value)} 
-            className="filter-input"
-          />
-          <input 
-            type="text" 
-            placeholder="Filter by Doctor" 
-            value={doctorFilter}
-            onChange={(e) => setDoctorFilter(e.target.value)}
-            className="filter-input" 
-          />
-             <input 
-            type="text" 
-            placeholder="Filter by state" 
-            value={stateFilter}
-            onChange={(e) => setStateFilter(e.target.value)}
-            className="filter-input" 
-          />
-        </div>
+        <div className="doctors">
+            {/* Filter section */}
+            <div className="filters">
+                <input
+                    type="text"
+                    placeholder="Filter by Date"
+                    value={dateFilter}
+                    onChange={(e) => setDateFilter(e.target.value)}
+                    className="filter-input"
+                />
+                <input
+                    type="text"
+                    placeholder="Filter by Doctor"
+                    value={doctorFilter}
+                    onChange={(e) => setDoctorFilter(e.target.value)}
+                    className="filter-input"
+                />
+                <input
+                    type="text"
+                    placeholder="Filter by state"
+                    value={stateFilter}
+                    onChange={(e) => setStateFilter(e.target.value)}
+                    className="filter-input"
+                />
+            </div>
 
-        {/* Perscriptions list */}
-        <div className='doctors'>
-          {perscriptions && perscriptions.map((perscription) => (
-            <PerscriptionDetails key={perscription._id} perscription={perscription} />
-          ))}
+            {/* Perscriptions list */}
+            <div className='doctors'>
+                {perscriptions  &&   perscriptions.map((perscription) => (
+                    <PerscriptionDetails  key=  {perscription._id} perscription={perscription} />
+                ))}
+            </div>
         </div>
-      </div>
-  )
-}
+    );
+};
 
 export default Perscription;
