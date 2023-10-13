3c0b15eb4c4123b6f2f1435593f3a458f53d32e2
AmrKhaledelsharkawy <amrk13415@gmail.com>
2023-10-14 01:09:30 +0300

    v2

--- backend/controllers/PatientController.js
+++ backend/controllers/PatientController.js
@@ -2,6 +2,7 @@ const { default: mongoose } = require('mongoose')
 
 const Patient = require('../models/Patient');
 const Perscriptions = require('../models/Perscriptions');
+const Doctor = require('../models/doctor')
 
 
 //Sign up as a new Patient
@@ -24,32 +25,78 @@ const signUp = async(req, res) => {
 }
 
 //View and Filter Perscriptions
+// const viewFilterPerscriptions = async (req, res) => {
+//     const user = req.session.user
+
+//     const patientID = user._id
+
+//     //const patientID = user.username
+
+
+//     const date = req.query.date;
+//     const doctor = req.query.doctor;
+//     const state = req.query.state
+
+//     let filter = {};
+
+//     if (patientID) filter.patientID = patientID
+//     if (doctor) filter.doctorID = doctor // Case-insensitive regex search
+//     if (date) filter.date_of_perscription = date;
+//     if (state) filter.state = state
+
+//     try {
+//         const prescriptions = await Perscriptions.find(filter);
+//         res.status(200).send(prescriptions);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// }
 const viewFilterPerscriptions = async (req, res) => {
-    const user = req.session.user
-
-    const patientID = user._id
-
-    //const patientID = user.username
-
-
+    const user = req.session.user;
+    const patientID = await user._id;
     const date = req.query.date;
-    const doctor = req.query.doctor;
-    const state = req.query.state
+    const doctorName = req.query.doctorName; // The input character(s) for doctor name search
+    const state = req.query.state;
 
     let filter = {};
+    let prescriptionsarray = [];
 
-    if (patientID) filter.patientID = patientID
-    if (doctor) filter.doctorID = doctor // Case-insensitive regex search
+    if (patientID) filter.patientID = patientID;
     if (date) filter.date_of_perscription = date;
-    if (state) filter.state = state
+    if (state) filter.state = state;
 
     try {
-        const prescriptions = await Perscriptions.find(filter);
-        res.status(200).send(prescriptions);
+        if (doctorName) {
+            // If a doctorName is provided, search for doctors that match the input
+            const matchingDoctors = await Doctor.find({
+                name: { $regex: new RegExp(doctorName, 'i') }
+            }, '_id'); // Only return doctor names
+            console.log(matchingDoctors)
+            for(let i=0;i<matchingDoctors.length;i++){
+               console.log(matchingDoctors[i])
+                const filter2  = {}
+                if (patientID) filter2.patientID = patientID;
+                if (date) filter2.date_of_perscription = date;
+                if (state) filter2.state = state;
+                if(matchingDoctors[i]) filter2.doctorID = matchingDoctors[i]
+                console.log(filter2)
+                const prescription = await Perscriptions.findOne(filter2)
+                prescriptionsarray.push(prescription)
+                
+            }
+            res.status(200).send( prescriptionsarray );
+
+        } else {
+            // If no doctorName is provided, retrieve prescriptions
+            const prescriptions = await Perscriptions.find(filter);
+            res.status(200).send( prescriptions );
+        }
     } catch (error) {
         res.status(400).send(error);
     }
 }
+
+
 
 const getSinglePerscription = async(req, res) => {
     const _id = req.params.perscID
