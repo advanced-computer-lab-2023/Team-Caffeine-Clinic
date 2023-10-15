02feed51940b08ea44e2aa29621ff8693b7b82d0
Ibrahim <ibrahimaboras2@gmail.com>
2023-10-15 05:40:54 +0300

    FINAL SUBMISSION

--- frontend/src/components/FamilyMemberDetails.js
+++ frontend/src/components/FamilyMemberDetails.js
@@ -2,7 +2,6 @@ const FamilyMemberDetails = ({ familyMember }) => {
   return (
     <div className="familyMember-details">
       <div className="name">{familyMember.name}</div>
-      <div className="details"><strong>patientID: </strong>{familyMember.patientID}</div>
       <div className="details"><strong>nationalID: </strong>{familyMember.nationalID}</div>
       <div className="details"><strong>age: </strong>{familyMember.age}</div>
       <div className="details"><strong>gender: </strong>{familyMember.gender}</div>
