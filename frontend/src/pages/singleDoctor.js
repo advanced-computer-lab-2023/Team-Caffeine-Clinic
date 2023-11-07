2b9fca5cedeb0a1b805ff1010546651140316efc
AmrKhaledelsharkawy <amrk13415@gmail.com>
2023-11-07 23:31:23 +0200

    tr

--- frontend/src/pages/singleDoctor.js
+++ frontend/src/pages/singleDoctor.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 const SingleDoctor = () => {
@@ -8,29 +8,51 @@ const SingleDoctor = () => {
   const [doctor, setDoctor] = useState(null);
   const [error, setError] = useState(null);
 
-  const {user} = useAuthContext()
+  const { user } = useAuthContext();
+
+  const handleCreateAppointment = async (date) => {
+    try {
+      const response = await fetch(`/api/patient/createAppointment/dusername=${doctor}&appointmentDate=${doctor.availableDates}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          date: date,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      // Handle successful response here
+      // e.g., show a success message or update the UI accordingly
+    } catch (error) {
+      console.error('Error creating appointment:', error);
+    }
+  };
 
   useEffect(() => {
     const fetchDoctor = async () => {
       try {
-        // console.log(username);
         const response = await fetch(`/api/doctors/getSingleDoctor/${username}`, {
           headers: {
-            'Authorization': `Bearer ${user.token}`
-          }
+            Authorization: `Bearer ${user.token}`,
+          },
         });
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
         setDoctor(data);
-        console.log(data.availableDates);
       } catch (error) {
         setError(error.message);
       }
     };
-    
-    if(user){
+
+    if (user) {
       fetchDoctor();
     }
   }, [username, user]);
@@ -45,29 +67,27 @@ const SingleDoctor = () => {
 
   return (
     <>
-    <Navbar />
-    <div className="single-doctor-details">
-      <div className="name">{doctor.name}</div>
-      <div className="details"><strong>speciality: </strong>{doctor.speciality}</div>
-      <div className="details"><strong>rate: </strong>{doctor.originalRate}</div>
-      <div className="details"><strong>affiliation: </strong>{doctor.affiliation}</div>
-      <div className="details"><strong>email: </strong>{doctor.email}</div>
-      <div className="details"><strong>education: </strong>{doctor.education}</div>
-      <div className="details"><strong>session price: </strong>{doctor.rateAfterDiscount + 0.1 * (doctor.rateAfterDiscount)}</div>
-      <div className="details">
-        <strong>Available Dates: </strong>
-        {doctor.availableDates && Array.isArray(doctor.availableDates) ? (
-          <ul>
-            {doctor.availableDates.map((Date, index) => (
-              <li key={index}>{Date}</li>
-            ))}
-          </ul>
-        ) : (
-          "No Dates available"
-        )}
+      <Navbar />
+      <div className="single-doctor-details">
+        {/* ... */}
+        <div className="details">
+          <strong>Available Dates: </strong>
+          {doctor.availableDates && Array.isArray(doctor.availableDates) ? (
+            <ul>
+              {doctor.availableDates.map((date, index) => (
+                <li key={index}>
+                  {date}{' '}
+                  <button onClick={() => handleCreateAppointment(date)}>
+                    Book Appointment
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            'No Dates available'
+          )}
+        </div>
       </div>
-    
-    </div>
     </>
   );
 };
