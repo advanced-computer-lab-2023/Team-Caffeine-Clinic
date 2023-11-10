070a89d8dd378419be72e23572a99a3c91a38f0b
Ibrahim <ibrahimaboras2@gmail.com>
2023-11-10 15:46:18 +0200

    small tweek

--- backend/controllers/PatientController.js
+++ backend/controllers/PatientController.js
@@ -80,12 +80,15 @@ function generateOTP() {
 
 const forgotPass = async(req, res) => {
     const {email} = req.body
-    
+    console.log(email);
     // Verify Valid Mail
-    const user = await Patient.findOne({email: email})
+    let user = await Patient.findOne({email: email})
     if(!user){
-        console.log('shit');
-        return res.status(400).json({err: "Email Address is incorrect"})
+        user = await Doctor.findOne({email: email})
+        
+        if(!user){
+            return res.status(400).json({err: "Email Address is incorrect"})
+        }
     }
 
     const randomOTP = generateOTP();
