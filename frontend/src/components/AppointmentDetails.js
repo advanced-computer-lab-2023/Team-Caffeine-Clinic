9dec85409ea9eb30360dd0e4e3740e8581ba132f
Ibrahim <ibrahimaboras2@gmail.com>
2023-12-19 01:00:34 +0200

    final

--- frontend/src/components/AppointmentDetails.js
+++ frontend/src/components/AppointmentDetails.js
@@ -8,7 +8,6 @@ import logo from '../img/hospital.jpg';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/esm/Button';
-import { Margin } from '@mui/icons-material';
 
 const AppointmentDetail = ({ appointment }) => {
   const [isOpen, setOpen] = useState(false);
@@ -167,10 +166,165 @@ const AppointmentDetail = ({ appointment }) => {
     <div className="doctor-details">
       <div className="name">
       </div>
-      <div className="details"><strong>Doctor: </strong>{appointment.doctor}</div>
-      <div className="details"><strong>Patient: </strong>{appointment.patient}</div>
+      <div className="details"><strong>Doctor: </strong>{appointment.doctor.name}</div>
+      <div className="details"><strong>Patient: </strong>{appointment.patient.name}</div>
       <div className="details"><strong>Date: </strong>{appointment.appointmentDate}</div>
       <div className="details"><strong>Status: </strong>{appointment.status}</div>
+
+      <button onClick={() => handleOpen()}>Add/Update Perscription</button>
+      <Popup closeOnDocumentClick={false} open={isOpen} modal nested contentStyle={contentStyle} lockScroll>
+        <div className="wrapper">
+          <div className="prescription_form">
+            <table className="prescription" border="1">
+              <tbody>
+                <tr height="15%">
+                  <td colSpan="2">
+                    <div className="header1">
+                      <div className="logo">
+                        <img src={logo} alt="Hospital Logo" />
+                      </div>
+                      <div className="credentials">
+                        <h4>{appointment.doctor.name}</h4>
+                        <h6>{appointment.doctor.speciality}</h6>
+                        <h6>{appointment.doctor.email}</h6>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="40%">
+                    <div className="disease_details">
+                      <div className="symptoms">
+                        <h4 className="d-header">Symptoms</h4>
+                        <ul className="symp" data-toggle="tooltip" >
+                          {symptom.map((symptom, index) => (
+                            <div>
+                              <li key={index}>
+                                <input
+                                  type="text"
+                                  value={symptom}
+                                  onChange={(e) => handleSymptomChange(index, e.target.value)}
+                                />
+                              </li>
+                              <span
+                                className="delete-icon"
+                                onClick={() => removeSymptomSlot(index)}
+                                role="button"
+                                tabIndex={0}
+                              >
+                                &#128465;
+                              </span>
+                            </div>
+                          ))}
+                        </ul>
+                        <Button className="add_symptom" onClick={addSymptom}>Add Symptom</Button>
+                      </div>
+                      <div className="tests">
+                        <h4 className="d-header">Tests</h4>
+                        <ul className="tst" data-toggle="tooltip" data-placement="bottom" title="Click to edit.">
+                          {tests.map((test, index) => (
+                            <div key={index}>
+                              <li>
+                                <input
+                                  type="text"
+                                  value={test}
+                                  onChange={(e) => handleTestChange(index, e.target.value)}
+                                />
+                              </li>
+                              <span
+                                className="delete-icon"
+                                onClick={() => removeTestSlot(index)}
+                                role="button"
+                                tabIndex={0}
+                              >
+                                &#128465;
+                              </span>
+                            </div>
+                          ))}
+                        </ul>
+                        <Button className="add_symptom" onClick={addTest}>Add Test</Button>
+                      </div>
+                      <div className="advice">
+                        <h4 className="d-header">Advice</h4>
+                        <input
+                          type="text"
+                          value={advice}
+                          onChange={(e) => setAdvice(e.target.value)}
+                          placeholder='Placeholder advice content.'
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td width="60%" valign="top">
+                    <span style={{ fontSize: '3em' }}>R<sub>x</sub></span> &nbsp;&nbsp;&nbsp; <b>Patient Name:</b> {appointment.patient.name}
+
+                    <hr />
+                    <div className="medicine">
+                      <section className="med_list">
+                        {medicine.map((medicine, index) => (
+                          <div className="med" key={index}>
+                            <ul>
+                              <li><h4>Medicine:</h4>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                  <b>Name:</b>
+                                  <Select
+                                    className='select_medicine'
+                                    options={medicineNames.map(name => ({ label: name, value: name }))}
+                                    value={{ label: medicine[index], value: medicine[index] }}
+                                    onChange={(selectedOption) => handleMedicineChange(index, selectedOption.value)}
+                                    styles={{
+                                      control: (provided) => ({
+                                        ...provided,
+                                        width: '200px', // Adjust the height as needed
+                                      }),
+                                    }}
+                                  />
+                                </div>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                  <b>Dosage:</b> <input
+                                    className="med_name"
+                                    placeholder="Enter Dosage"
+                                    value={dosage[index]}
+                                    onChange={(e) => handleDosageChange(index, e.target.value)}
+                                  /></div>
+                                <span
+                                  className="delete-icon"
+                                  onClick={() => removeMedicineSlot(index)}
+                                  role="button"
+                                  tabIndex={0}
+                                >
+                                  &#128465;
+                                </span>
+                              </li>
+                            </ul>
+                          </div>
+                        ))}
+                      </section>
+                      <div
+                        id="add_med"
+                        data-toggle="tooltip"
+                        data-placement="right"
+                        title="Click anywhere on the blank space to add more."
+                        onClick={addMedicineSlot}
+                      >
+                        Click to add...
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="button_group">
+
+              {/* <button className="pdf_prescription btn btn-danger">PDF</button> */}
+            </div>
+            <div id="snacking">Saving...</div>
+            <div id="snacked">Saved!</div>
+          </div>
+        </div>
+        <Button onClick={() => handleAddAppointment()}>Add/Update</Button>
+        <Button onClick={() => handleOpen()}>Close</Button>
+      </Popup>
     </div>
   )
 }
--- frontend/src/pages/AppointmentDoc.js
+++ frontend/src/pages/AppointmentDoc.js
@@ -3,16 +3,18 @@ import AppointmentDetail from '../components/AppointmentDetails';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 const AppointmentDoc = () => {
-    const [appointments, setAppointments] = useState(null);
-    const [date, setDate] = useState('');
-    const [status, setStatus] = useState('');
-    const { user } = useAuthContext();
+  const [appointments, setAppointments] = useState(null);
+  const [date, setDate] = useState('');
+  const [status, setStatus] = useState('');
+  const { user } = useAuthContext();
 
-    const margin = {
-        marginTop: '100px',
-    }
+  const margin = {
+    marginTop: '100px',
+  }
 
-    useEffect(() => {
+  useEffect(() => {
+    let formattedDate = null;
+    if(date){
     const selectedDate = new Date(date);
     const year = selectedDate.getFullYear();
     const month = selectedDate.getMonth() + 1; // Adding 1 because getMonth() returns 0-indexed months
@@ -20,110 +22,120 @@ const AppointmentDoc = () => {
     const hours = selectedDate.getHours();
     const minutes = selectedDate.getMinutes();
 
-    const formattedDate = `${year}\\${month}\\${day}:${hours}:${minutes}`;
-        const fetchAppointments = async (e) => {
-            try {
-                const response = await fetch(
-                    `api/doctorInfo/appointments?date=${formattedDate}&status=${status}`,
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            Authorization: `Bearer ${user.token}`,
-                        },
-                    }
-                );
-                console.log("dateFilter");
+    formattedDate = `${year}\\${month}\\${day}:${hours}:${minutes}`;
+    }
+    
+    const fetchAppointments = async (e) => {
+      let url = 'api/doctorInfo/appointments';
 
-                if (response.ok) {
-                    const data = await response.json();
-                    setAppointments(data);
-                } else {
-                    // Handle error here
-                }
-            } catch (error) {
-                console.error('Error fetching appointments:', error);
-            }
-        };
-    }, [date, status, user]);
+      const params = new URLSearchParams();
+      if (date) params.append('date', formattedDate);
+      if (status) params.append('status', status);
+      if (params.toString()) url += `?${params.toString()}`;
 
-    return (
-        // <div className="doctors">
-        //     {/* Filter section */}
-        //     <div className="filters">
-        //         <input
-        //             type="text"
-        //             placeholder="Filter by Date"
-        //             value={date}
-        //             onChange={(e) => setDate(e.target.value)}
-        //             className="filter-input"
-        //         />
-        //         <select value={status} onChange={(e) => setStatus(e.target.value)} className="filter-input">
-        //             <option value="">--Select Status--</option>
-        //             <option value="upcoming">Upcoming</option>
-        //             <option value="completed">Completed</option>
-        //             <option value="cancelled">Cancelled</option>
-        //             <option value="rescheduled">Rescheduled</option>
-        //             <option value="FollowUp">FollowUp</option>
-        //         </select>
-        //     </div>
+      try {
+        const response = await fetch(
+          url,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        );
+        console.log("dateFilter");
 
-        //     {/* Appointments list */}
-        //     <div className="appointments">
-        //         {appointments &&
-        //             appointments.map((appointment) => (
-        //                 <AppointmentDetail key={appointment._id} appointment={appointment} />
-        //             ))}
-        //     </div>
-        // </div>
-        <div>
-  <section className="breadcrumbs">
-    <div className="container">
-      <div className="d-flex justify-content-between align-items-center">
-        <h2>View All Upcoming Appointments</h2>
-        <ol>
-          <li><a href="index.html">Home</a></li>
-          <li>All Upcoming Appointments</li>
-        </ol>
-      </div>
-    </div>
-  </section>
-  
-  <section className="inner-page">
-    <div className="container">
-    <section id="faq" className="faq section-bg">
-  <div className="container">
-    <div className="section-title">
-      <h2>All Upcoming Appointments</h2>
-    </div>
-             <div className="filters">
+        if (response.ok) {
+          const data = await response.json();
+          setAppointments(data);
+        } else {
+          // Handle error here
+        }
+      } catch (error) {
+        console.error('Error fetching appointments:', error);
+      }
+    };
+    fetchAppointments();
+  }, [date, status, user]);
+
+  return (
+    // <div className="doctors">
+    //     {/* Filter section */}
+    //     <div className="filters">
+    //         <input
+    //             type="text"
+    //             placeholder="Filter by Date"
+    //             value={date}
+    //             onChange={(e) => setDate(e.target.value)}
+    //             className="filter-input"
+    //         />
+    //         <select value={status} onChange={(e) => setStatus(e.target.value)} className="filter-input">
+    //             <option value="">--Select Status--</option>
+    //             <option value="upcoming">Upcoming</option>
+    //             <option value="completed">Completed</option>
+    //             <option value="cancelled">Cancelled</option>
+    //             <option value="rescheduled">Rescheduled</option>
+    //             <option value="FollowUp">FollowUp</option>
+    //         </select>
+    //     </div>
+
+    //     {/* Appointments list */}
+    //     <div className="appointments">
+    //         {appointments &&
+    //             appointments.map((appointment) => (
+    //                 <AppointmentDetail key={appointment._id} appointment={appointment} />
+    //             ))}
+    //     </div>
+    // </div>
+    <div>
+      <section className="breadcrumbs">
+        <div className="container">
+          <div className="d-flex justify-content-between align-items-center">
+            <h2>View All Upcoming Appointments</h2>
+            <ol>
+              <li><a href="index.html">Home</a></li>
+              <li>All Upcoming Appointments</li>
+            </ol>
+          </div>
+        </div>
+      </section>
+
+      <section className="inner-page">
+        <div className="container">
+          <section id="faq" className="faq section-bg">
+            <div className="container">
+              <div className="section-title">
+                <h2>All Upcoming Appointments</h2>
+              </div>
+              <div className="filters">
                 <input
-                    type="datetime-local"
-                    placeholder="Filter by Date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                    className="form-control"
+                  type="datetime-local"
+                  placeholder="Filter by Date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                  className="form-control"
                 />
                 <select value={status} onChange={(e) => setStatus(e.target.value)} className="form-select">
-                    <option value="">--Select Status--</option>
-                    <option value="upcoming">Upcoming</option>
-                    <option value="completed">Completed</option>
-                    <option value="cancelled">Cancelled</option>
-                    <option value="rescheduled">Rescheduled</option>
-                    <option value="FollowUp">FollowUp</option>
+                  <option value="">--Select Status--</option>
+                  <option value="upcoming">Upcoming</option>
+                  <option value="completed">Completed</option>
+                  <option value="cancelled">Cancelled</option>
+                  <option value="rescheduled">Rescheduled</option>
+                  <option value="FollowUp">FollowUp</option>
                 </select>
                 <br />
-            </div>
+              </div>
 
-    <div className="faq-list">
-    {appointments && appointments.map((appointment) => (
-     <AppointmentDetail key={appointment._id} appointment={appointment} />
-            ))}
+              <div className="faq-list">
+                {appointments && appointments.map((appointment) => (
+                  <AppointmentDetail key={appointment._id} appointment={appointment} />
+                ))}
+              </div>
+            </div></section>
         </div>
-  </div></section>
+      </section>
     </div>
-  </section>
-</div>
-    );
+  );
 };
 
 export default AppointmentDoc;
